d9e3cb6dd5804b046001085c45762cf874783c46
Yueun Kim <yueun.owl@gmail.com>
2022-12-14 15:48:43 -0700

    retrieved raw routes data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Routes from "./components/routes.js"
+import data from "./data.js";
 
-const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+const App = () => {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        {data.routes.map(route => (
+            <>
+            <li>Airline: {route.airline}</li>
+            <li>Source: {route.src}</li>
+            <li>Destination: {route.dest}</li>
+            </>
+          )
+        )}
+        <p>welcome!</p>
+      </section>
+    </div>
+  )
+}
 
 export default App;
---
+++ src/components/routes.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// display 
+const Routes = ({ airline }) => {
+  return <li>the first airline is: {airline}</li>
+}
+
+export default Routes;
