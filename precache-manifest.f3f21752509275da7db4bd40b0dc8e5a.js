62afa2a34796d4208cacb74a8626cfb262f37d96
Yueun Kim <yueun.owl@gmail.com>
2022-12-21 21:34:37 -0700

    Updates

---
+++ precache-manifest.f3f21752509275da7db4bd40b0dc8e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2010c4b118bc27cc34c959fe7ad2379",
+    "url": "/airline-routes/index.html"
+  },
+  {
+    "revision": "b2cf0f489406e339f5f1",
+    "url": "/airline-routes/static/css/main.da45a1f1.chunk.css"
+  },
+  {
+    "revision": "765249f4d2d5cb538a8f",
+    "url": "/airline-routes/static/js/2.d7eb0cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airline-routes/static/js/2.d7eb0cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cf0f489406e339f5f1",
+    "url": "/airline-routes/static/js/main.e60052f8.chunk.js"
+  },
+  {
+    "revision": "a30a2a5ca68d46378471",
+    "url": "/airline-routes/static/js/runtime-main.91222637.js"
+  }
+]);
